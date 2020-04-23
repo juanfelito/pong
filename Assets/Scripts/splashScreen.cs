b9f5cf4eaf5cf2bd50f027cf26e5374ca63e481b
Felipe Balcero <felipe.balcero45@gmail.com>
2020-04-23 01:48:12 -0500

    Pong complete

---
+++ Assets/Scripts/splashScreen.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class splashScreen : MonoBehaviour {
+    public string sceneToLoad;
+    public int secondsToWait;
+
+    void Start() {
+        Invoke("OpenNextScene", secondsToWait);
+    }
+
+    void OpenNextScene() {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+}
